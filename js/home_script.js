b5c18f3ba97e91fb4e3bf33ad6daa4684b182677
Chinmay Patil <chinmay412@gmail.com>
2014-06-10 22:40:15 +0530

    First commit with very basic structure for index page

---
+++ js/home_script.js
@@ -0,0 +1,31 @@
+// $(document).ready(function(){
+
+// 	var winW = $(window).width();
+// 	var winH = $(window).height();
+
+// 	var row1W = winW;
+// 	var row1H = winH * 0.85;
+
+// 	var row2W = winW;
+// 	var row2H = winH * 0.15;
+
+// 	$(".bodySection").css("height",row1H);
+// 	$(".footerSection").css("height",row2H);
+
+// 	// Side Navigation
+// 	$(".span3").css("height",row1H);
+
+// 	// Main Images Section
+// 	$(".span8").css("height",row1H);
+// 	$(".span8").css("width",row1H * 1.4);
+
+// 	$(window).resize(function(){
+
+// 		$(".span3").css("height",row1H);
+
+// 		$(".span8").css("height",row1H);
+// 		$(".span8").css("width",row1H * 1.4);
+
+// 	});
+
+// });
