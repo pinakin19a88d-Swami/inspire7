ae98fb18621b12a4f007445314268c827bb46243
Chinmay Patil <chinmay412@gmail.com>
2014-06-13 15:00:31 +0530

    workable Grid

--- js/home_script.js
+++ js/home_script.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	var winW = $(window).width();
-	var winH = $(window).height();
+// 	var winW = $(window).width();
+// 	var winH = $(window).height();
 
-	var row1W = winW;
-	var row1H = winH * 0.85;
+// 	var row1W = winW;
+// 	var row1H = winH * 0.85;
 
-	var row2W = winW;
-	var row2H = winH * 0.15;
+// 	var row2W = winW;
+// 	var row2H = winH * 0.15;
 
-	$(".bodySection").css("height",row1H);
-	$(".footerSection").css("height",row2H);
+// 	$(".bodySection").css("height",row1H);
+// 	$(".footerSection").css("height",row2H);
 
-	// Side Navigation
-	$(".span3").css("height",row1H);
+// 	// Side Navigation
+// 	$(".span3").css("height",row1H);
 
-	// Main Images Section
-	$(".span8").css("height",row1H);
-	$(".span8").css("width",row1H * 1.4);
+// 	// Main Images Section
+// 	$(".span8").css("height",row1H);
+// 	$(".span8").css("width",row1H * 1.4);
 
-	$(window).resize(function(){
+// 	$(window).resize(function(){
 
-		$(".span3").css("height",row1H);
+// 		$(".span3").css("height",row1H);
 
-		$(".span8").css("height",row1H);
-		$(".span8").css("width",row1H * 1.4);
+// 		$(".span8").css("height",row1H);
+// 		$(".span8").css("width",row1H * 1.4);
 
-	});
+// 	});
 
-});
+// });
