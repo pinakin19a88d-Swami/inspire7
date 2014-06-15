029c8f7ebfdde814c9a51c95d68db50f3e8afe8c
Pinakin-swami <pinakindesigns@gmail.com>
2014-06-15 18:46:32 +0530

    active class added dynamicaly, page loaded dynamicalt

--- js/ajax_load_script.js
+++ js/ajax_load_script.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
 
-	$("#whoWeAreNav").click(function() {
-		$(".blackBox").load("ajax/who_we_are.html");
-		// $("ul > li").removeClass("active");
-		// $("#whoWeAreNav").addClass("active");
-		$("#whoWeAreNav").css("color","#cc0000");
-		$(".gridCurtain").css("display","block");
-	});
+	$(".subMenu li a").click(function() {
+		$(".subMenu li a").removeClass("active");
+		$(this).addClass("active");
 
-	$("#whatWeDoNav").click(function() {
-		$(".blackBox").load("ajax/what_do_we_do.html");
-		// $("ul > li").removeClass("active");
-		// $("#whoWeAreNav").addClass("active");
-		$("#whatWeDoNav").css("color","#cc0000");
+		var current = $(this).attr("name");
+		$(".blackBox").load(current);
 		$(".gridCurtain").css("display","block");
-	});
+	});	
 
 });
 
